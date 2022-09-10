d2860c9777954af15e2f09e2068c683f92b5104d
Pacmega <bas.witters@hotmail.com>
2022-09-10 22:55:15 +0200

    KNN code for classification and regression implemented

--- practicals/code/knn.py
+++ practicals/code/knn.py
@@ -1,7 +1,14 @@
-class KnnClassifier(object):
-    def __init__(self, train_data):
-        super(Knearestneighbours, self).__init__()
-        self.train_data = train_data
+from scipy.spatial.distance import cdist
+import numpy as np
+
+from scipy.spatial.distance import cdist
+import numpy as np
+
+class KnnBase(object):
+    def __init__(self, train_data, train_targets):
+        self.train_data = self.normalise_dataset(train_data)
+        self.targets = train_targets
+
 
     def normalise_dataset(self, dataset):
         def normalise(feature):
@@ -11,13 +18,38 @@ class KnnClassifier(object):
         
         return np.apply_along_axis(normalise, axis=0, arr=dataset)
 
-    def predict_new_data(self, new_data, k):
-        # For each data point,
-        # calculate Euclidean distance to all other points
+
+    def get_k_nearest_targets(self, new_data, k):
+        # For each data point calculate Euclidean distance to all other points
+        transformed_data = self.normalise_dataset(new_data)
+        distances = cdist(transformed_data, self.train_data)
+        
         # Take k nearest points, get their target values
-        # take most common target value (for reg, this is "take mean")
-        # Return that value
-        raise NotImplementedError(':)')
+        k_nearest_indices = np.argsort(distances, axis=0)[:,:k]
+        k_nearest_classes = self.targets[k_nearest_indices]
+
+        # Take most common target value for each point and return that value
+        return k_nearest_classes
+
+
+class KnnClassifier(KnnBase):
+    def __init__(self, train_data, train_targets):
+        super().__init__(train_data, train_targets)
+    
+    
+    def predict_classes(self, new_data, k):
+        nearest_targets = self.get_k_nearest_targets(new_data, k)
+        return np.round(np.mean(nearest_targets, axis=1))
+    
+    
+class KnnRegressor(KnnBase):
+    def __init__(self, train_data, train_targets):
+        super().__init__(train_data, train_targets)
+    
+    
+    def predict_regression(self, new_data, k):
+        nearest_targets = self.get_k_nearest_targets(new_data, k)
+        return np.mean(nearest_targets, axis=1)
 
 # def knn(new_data, k):
 #     predicted_labels = []
