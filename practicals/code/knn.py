c46110c18eb7ae4c786c17d60ed9b80e06e9be97
Pacmega <bas.witters@hotmail.com>
2022-09-11 22:28:52 +0200

    Added notebook, removed old code

--- practicals/code/knn.py
+++ practicals/code/knn.py
@@ -50,34 +50,3 @@ class KnnRegressor(KnnBase):
     def predict_regression(self, new_data, k):
         nearest_targets = self.get_k_nearest_targets(new_data, k)
         return np.mean(nearest_targets, axis=1)
-
-# def knn(new_data, k):
-#     predicted_labels = []
-    
-#     for point in new_data:
-#         nearest_points = []
-#         index = 0
-#         for i in range(diabetes_X.shape[0]):
-#             # Euclidean distance
-#             dist_glucose = (diabetes_X[i,0]-point[0])**2
-#             dist_bloodpressure = (diabetes_X[i,1]-point[1])**2
-#             distance = np.sqrt(dist_glucose + dist_bloodpressure)
-
-#             if len(nearest_points) != k:
-#                 # Still populating the list of k nearest neighbours
-#                 nearest_points.append((distance, diabetes_y[i]))
-#                 nearest_points.sort(key=lambda neighbour: neighbour[0])
-#             elif nearest_points[-1][0] > distance:
-#                 # Current observed data point is closer than furthest away
-#                 #   previously known one. Replace and re-sort.
-#                 nearest_points[-1] = (distance, diabetes_y[i])
-#                 nearest_points.sort(key=lambda neighbour: neighbour[0])
-#             # Else: this point was not close enough to be relevant for knn
-
-#         # As the target label is binary, we can shortcut the label decision process
-#         # Note: 0.5 is rounded to 0 by Python
-#         nearest_labels = np.array(nearest_points)[:,1]
-#         most_common_label = round(np.mean(nearest_labels))
-#         predicted_labels.append(most_common_label)
-        
-#     return predicted_labels
